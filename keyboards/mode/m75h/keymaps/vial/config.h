b69fb6005a89648166d0de07b40ce265cd143097
Jason <jason_augustimas@yahoo.com>
2023-08-02 16:46:41 +0200

    Updated Sonnet vial on 02/08/2023

--- keyboards/mode/m75h/keymaps/vial/config.h
+++ keyboards/mode/m75h/keymaps/vial/config.h
@@ -1,8 +1,23 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ Copyright 2020 Álvaro "Gondolindrim" Volpato <alvaro.volpato@usp.br>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0xF0, 0x43, 0xB4, 0x31, 0xB5, 0x0C, 0x27, 0x6F}
+#define VIAL_KEYBOARD_UID {0xAD, 0xA1, 0xEB, 0xB9, 0x18, 0x79, 0xD6, 0x41}
 
 /* Unlock with esc / del */
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
